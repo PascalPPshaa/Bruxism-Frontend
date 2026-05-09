e94c7bcbd9606b26399daec7803e84ec698ed8d6
PascalPPshaa <pascalpahlevi9@gmail.com>
2026-05-09 15:30:45 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        // Jika frontend memanggil URL yang diawali /api-backend/
+        source: '/api-backend/:path*',
+        // Maka Next.js akan meneruskannya ke ngrok
+        destination: 'https://85a1-103-3-222-190.ngrok-free.app/api/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
